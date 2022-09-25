6e6159f2e9d0ee9f9dee1365b381d7498017c3c9
Richard Kuo <rkuo2000@gmail.com>
2022-09-25 10:38:13 +0800

    Add files via upload

---
+++ Web/cast.sh
@@ -0,0 +1,8 @@
+# pip install pychromecast
+# pip install catt
+# Usage:
+
+#       catt cast "https://youtu.be/LzBUm31Vn3k"
+#       catt cast ./myvideo.mp4
+#       catt cast_site https://github.com/rkuo2000/Robotics
+catt cast "https://youtu.be/LzBUm31Vn3k"
